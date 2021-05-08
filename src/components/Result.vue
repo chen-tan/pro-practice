01574d8754e7d36f22529a4a45eea8cb9d2626c3
chentan <chentan@hua-cloud.com.cn>
2021-05-08 09:49:09 +0800

    add result in master

---
+++ src/components/Result.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Only when you change the code in master,you will meet conflict when you use "git pull origin master"
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
