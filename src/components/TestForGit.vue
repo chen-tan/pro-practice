90c0dfe64647da0d50862060fd4e331a6b296e66
chentan <chentan@hua-cloud.com.cn>
2021-05-19 10:01:32 +0800

    test for merge change in branch 6.

--- src/components/TestForGit.vue
+++ src/components/TestForGit.vue
@@ -1,14 +1,7 @@
 <template>
   <div class="test-for-git">
-      This is 1.pull
-      This is 2.pull
-      This is 3.pull
-      This is 4.pull
-      This is 5.pull
-
-      This is an add in 6.create from 1.pull
-      This is an add after create branch 6.
-      This is a test for merge in 1
+      test for merge
+      change in branch 6.
   </div>
 </template>
 
